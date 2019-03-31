693de54e9292d4353d64cd78753a6e012715286b
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2019-03-31 13:10:50 -0600

    Cambio para que la consulta rest a hospitales pase por servlet

--- src/main/webapp/js/aseguradosH.js
+++ src/main/webapp/js/aseguradosH.js
@@ -2,7 +2,7 @@ $(document).ready(
         function() {
             $.ajax({
                 type: 'GET',
-                url: 'http://localhost:8080/proyectoDB2-Hospitales/restCo/cobertura/getCobertura',
+                url: 'http://localhost:8080/proyectoDB2-Hospitales/getCobertura',
                 dataType: 'json',
                 success: function(data) {
                     var $pData = $('#datosS');
@@ -25,3 +25,4 @@ $(document).ready(
                 }
             });
 });
+
