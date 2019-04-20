edf3a6d6655920159c63597286bbbb068e06ea50
manupianista <manuelecastilloo@gmail.com>
2019-04-20 03:45:23 -0600

    avanzando con el agregado de pacientes desde hospital

---
+++ src/main/webapp/js/clienteList.js
@@ -0,0 +1,23 @@
+$(document).ready(
+        function() {
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:8080/proyectoDB2-seguro/GetPatient',
+                dataType: 'json',
+                success: function(data) {
+                    var $pData = $('#patients');
+                    $pData.empty();
+                    for (var i = 0; i < data.length; i++) {
+                        $pData.append("<option value="+data[i].id+">"+data[i].nombre+" "+data[i].apellido+"</option>");
+                    }
+                    if(data.length==0){
+                        $pData.append("<p>No hay datos disponibles</p>");
+                    }
+                },
+                error : function() {
+                    var $pData = $('#patientData');
+                    $pData.empty();
+                    $pData.append("<p>No hay datos disponibles</p>");
+                }
+            });
+});
