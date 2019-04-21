e51fa2fdc6453e39dedef8e2ca74eb38056352dd
manupianista <manuelecastilloo@gmail.com>
2019-04-20 19:45:17 -0600

    ya funciona la verificacion de que exista el paciente22

--- src/main/java/gio/co/seguros/verificarPH.java
+++ src/main/java/gio/co/seguros/verificarPH.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.*;
 import com.google.gson.*;
 import java.io.File;
+import java.net.URI;
+import javax.ws.rs.core.Response;
 /*import com.google.gson.GsonBuilder;*/
 
 
@@ -48,50 +50,91 @@ public class verificarPH extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            String url22, urlAdd;
+            String urlH, urlAdd, urlCliente;
             String parDPI = request.getParameter("pId");
             String parServ = request.getParameter("servicioId");
-            String parHosp = request.getParameter("hospnum");
-            
+            int parHosp = Integer.parseInt(request.getParameter("hospnum"));
+            parHosp=1;
             //out.println(hayono);
             //out.println(parDPI);            
             //out.println(parServ);
+            urlAdd = "http://localhost:8080/proyectoDB2-seguro/AddPatient";
+            urlCliente = "http://localhost:8080/proyectoDB2-seguro/restC/cliente/getCliente?dpi="+parDPI;
             
             double DPIcliente = Double.parseDouble(parDPI);
-            url22 = "http://localhost:8080/proyectoDB2-Hospitales/restP/patient/getPatientDPI?dpi="+DPIcliente;
             
-            urlAdd = "http://localhost:8080/proyectoDB2-seguro/AddPatient";
-
-            URL obj = new URL(url22);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-           
+            switch (parHosp){
+                case 1:
+                        urlH = "http://localhost:8080/proyectoDB2-Hospitales/restP/patient/getPatientDPI?dpi="+DPIcliente;
+                    break;
+                case 2:
+                        urlH = "http://localhost:8080/proyectoDB2-Hospitales2/restP/patient/getPatientDPI?dpi="+DPIcliente;
+                    break;
+                case 3:
+                        urlH = "http://localhost:8080/proyectoDB2-Hospitales3/restP/patient/getPatientDPI?dpi="+DPIcliente;
+                    break;
+                default:
+                        urlH = "http://localhost:8080/proyectoDB2-Hospitales/restP/patient/getPatientDPI?dpi="+DPIcliente;
+                    break;
+            }
+            
+              
+            //-----------------------Para obtener el JSON de GetPatient del rest de hospitales-----------------------
+            
+            URL obj = new URL(urlH);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();           
             int responseCode = con.getResponseCode();
-            //out.println("Response code: "+responseCode);
-            
+            //out.println("Response code: "+responseCode);            
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(con.getInputStream()));
-            out.println(con);
+            //out.println(con);
             String inputLine;
             StringBuffer response2 = new StringBuffer();
             while ((inputLine = in.readLine()) != null) {
                
                 response2.append(inputLine); 
-            }
+            }          
             
+            String respuesta = response2.toString(); 
             
-            String respuesta = response2.toString();         
-            {
+            //-----------------------Para obtener el JSON de clientes de la aseguradora-----------------------
+            URL obj2 = new URL(urlCliente);
+            HttpURLConnection con2 = (HttpURLConnection) obj2.openConnection();           
+            int responseCode2 = con2.getResponseCode();
+            //out.println("Response code: "+responseCode);            
+            BufferedReader in2 = new BufferedReader(
+                    new InputStreamReader(con2.getInputStream()));
+            //out.println(con);
+            String inputLine2;
+            StringBuffer response22 = new StringBuffer();
+            while ((inputLine2 = in2.readLine()) != null) {
+               
+                response22.append(inputLine2); 
+            }          
             
+            String respuesta2 = response22.toString();
+            
+            {            
             if(respuesta.length()>2){
-            out.println(respuesta);
+            //out.println(respuesta);
+            
+            
+            response.sendRedirect("aggregarC_h.jsp?dpi="+parDPI+"&hosp="+parHosp+"&servicioId="+parServ);
             } 
             else {
                 out.println("no existe el men");
+                //out.println(response22);
+                
+                
+                
+                
             }
             }
-
             in.close();
            
+            
+            
+            
             
         } catch (Exception e) {
             System.err.println(e);
@@ -100,6 +143,38 @@ public class verificarPH extends HttpServlet {
     
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
