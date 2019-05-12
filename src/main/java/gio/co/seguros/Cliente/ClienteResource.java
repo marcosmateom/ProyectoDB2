cd6b9ac83bca9963081625a0da05234acc82618b
Miguel Gonzalez <miguelgon_8@hotmail.com>
2019-05-12 00:48:29 -0600

    trabajando en vista clientes

--- src/main/java/gio/co/seguros/Cliente/ClienteResource.java
+++ src/main/java/gio/co/seguros/Cliente/ClienteResource.java
@@ -23,124 +23,104 @@ import org.bson.conversions.Bson;
  *
  * @author C.V
  */
-
 @Path("/cliente")
 
 public class ClienteResource {
-    
-        protected List<Document> clientsList;// = new List<Document>();
-        @GET
-        @Path("/getCliente")
-        @Produces(MediaType.APPLICATION_JSON)
-        public List<Document> getCliente(
-            @QueryParam("dpi") String cId){
+
+    protected List<Document> clientsList;// = new List<Document>();
+
+    @GET
+    @Path("/getCliente")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Document> getCliente(
+            @QueryParam("dpi") String cId) {
         //Obtener # del hospital
         makeList(cId);
-        
+
         return clientsList;
-        }
-    
-        protected void makeList(String cId){
-        
+    }
+
+    protected void makeList(String cId) {
+
         //Conexion con db oracle
-        
-            //Response info
-            try{
-                
-                MongoCollection<Document> coll = gio.co.seguros.collClientes.collclientes();
-                
-                if(cId!=null){
-                    //Query con el filtro para seleccionar un paciente
-                    //sql = "select * from pacientes where paciente_id ="+pId+" order by paciente_id";
-                    List<? extends Bson> pipeline = Arrays.asList(
-                    new Document()
-                            .append("$match", new Document()
-                                    .append("documentoIdentificacion", cId)
-                            ), 
-                    new Document()
-                            .append("$lookup", new Document()
-                                    .append("from", "polizas")
-                                    .append("localField", "tipo_poliza")
-                                    .append("foreignField", "tipo_poliza")
-                                    .append("as", "join")
-                            ), 
-                    new Document()
-                            .append("$replaceRoot", new Document()
-                                    .append("newRoot", new Document()
-                                            .append("$mergeObjects", Arrays.asList(
-                                                    new Document()
-                                                            .append("$arrayElemAt", Arrays.asList(
-                                                                    "$join",
-                                                                    0.0
+        //Response info
+        try {
+
+            MongoCollection<Document> coll = gio.co.seguros.collClientes.collclientes();
+
+            if (cId != null) {
+                //Query con el filtro para seleccionar un paciente
+                //sql = "select * from pacientes where paciente_id ="+pId+" order by paciente_id";
+                List<? extends Bson> pipeline = Arrays.asList(
+                        new Document()
+                                .append("$match", new Document()
+                                        .append("documentoIdentificacion", cId)
+                                ),
+                        new Document()
+                                .append("$lookup", new Document()
+                                        .append("from", "polizas")
+                                        .append("localField", "tipo_poliza")
+                                        .append("foreignField", "tipo_poliza")
+                                        .append("as", "join")
+                                ),
+                        new Document()
+                                .append("$replaceRoot", new Document()
+                                        .append("newRoot", new Document()
+                                                .append("$mergeObjects", Arrays.asList(
+                                                        new Document()
+                                                                .append("$arrayElemAt", Arrays.asList(
+                                                                        "$join",
+                                                                        0.0
                                                                 )
-                                                            ),
-                                                    "$$ROOT"
+                                                                ),
+                                                        "$$ROOT"
                                                 )
-                                            )
-                                    )
-                            )
-            );
-                    List<Document> clientes = (List<Document>) coll.aggregate(pipeline).into( new ArrayList<Document>());
-                    clientsList = clientes;
-                }
-                else{
-                    //Query de todos los pacientes
-                    //MongoCollection<Document> coll = gio.co.seguros.collPoliza.collpoliza();
-                    List<? extends Bson> pipeline = Arrays.asList(
-                    new Document()
-                            .append("$lookup", new Document()
-                                    .append("from", "polizas")
-                                    .append("localField", "tipo_poliza")
-                                    .append("foreignField", "tipo_poliza")
-                                    .append("as", "join")
-                            ), 
-                    new Document()
-                            .append("$replaceRoot", new Document()
-                                    .append("newRoot", new Document()
-                                            .append("$mergeObjects", Arrays.asList(
-                                                    new Document()
-                                                            .append("$arrayElemAt", Arrays.asList(
-                                                                    "$join",
-                                                                    0.0
+                                                )
+                                        )
+                                )
+                );
+                List<Document> clientes = (List<Document>) coll.aggregate(pipeline).into(new ArrayList<Document>());
+                clientsList = clientes;
+            } else {
+                //Query de todos los pacientes
+                //MongoCollection<Document> coll = gio.co.seguros.collPoliza.collpoliza();
+                List<? extends Bson> pipeline = Arrays.asList(
+                        new Document()
+                                .append("$lookup", new Document()
+                                        .append("from", "polizas")
+                                        .append("localField", "tipo_poliza")
+                                        .append("foreignField", "tipo_poliza")
+                                        .append("as", "join")
+                                ),
+                        new Document()
+                                .append("$replaceRoot", new Document()
+                                        .append("newRoot", new Document()
+                                                .append("$mergeObjects", Arrays.asList(
+                                                        new Document()
+                                                                .append("$arrayElemAt", Arrays.asList(
+                                                                        "$join",
+                                                                        0.0
                                                                 )
-                                                            ),
-                                                    "$$ROOT"
+                                                                ),
+                                                        "$$ROOT"
                                                 )
-                                            )
-                                    )
-                            )
-            );
-                    List<Document> clientes = (List<Document>) coll.aggregate(pipeline).into( new ArrayList<Document>());
-                    clientsList = clientes;
-                }
-                
-                /*MongoCollection<Document> coll = gio.co.seguros.collClientes.collclientes();
+                                                )
+                                        )
+                                )
+                );
+                List<Document> clientes = (List<Document>) coll.aggregate(pipeline).into(new ArrayList<Document>());
+                clientsList = clientes;
+            }
+
+            /*MongoCollection<Document> coll = gio.co.seguros.collClientes.collclientes();
                 List<Document> clientes = (List<Document>) coll.find().into( new ArrayList<Document>());
                 clientsList = clientes;*/
-                //String a = clientes.get(0).toJson();
-                //String a2 = a;
+            //String a = clientes.get(0).toJson();
+            //String a2 = a;
+        } catch (MongoException e) {
+            System.err.println(e);
+        }
 
-                
-            }catch (MongoException e) {
-                System.err.println(e);
-            }
-            
     }
-    
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
